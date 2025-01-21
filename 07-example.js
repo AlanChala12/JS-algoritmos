b45ae00743c7ddaf869c60a1af24c9d8fa765b7e
Alan Chala <alan.chala12@gmail.com>
2025-01-21 10:19:45 -0500

    Mostrar en pantalla la tabla de multiplicar del número 5.

---
+++ 07-example.js
@@ -0,0 +1,10 @@
+// Mostrar en pantalla la tabla de multiplicar del número 5.
+
+let tableOf = 5
+
+for ( let i = 1; i < 11; i++ ) {
+    
+  let multi = 5 * i
+  console.log(`${tableOf} x ${i} = ${multi}`)
+  
+}
