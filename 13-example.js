f560cecc330b6cd5e70a6b629fe84271bc7faa74
Alan Chala <alan.chala12@gmail.com>
2025-01-30 13:27:57 -0500

    Escribe un programa que imprima el cuadrado de los números enteros desde 0 hasta un número entero n dado.

---
+++ 13-example.js
@@ -0,0 +1,12 @@
+// Escribe un programa que imprima el cuadrado de los números enteros desde 0 hasta un número entero n dado.
+
+let n = 5
+
+for (let i = 0; i < n; i++) {
+
+  let cuadrado = 0
+  cuadrado = i ** 2
+  
+  console.log(cuadrado)
+
+}
