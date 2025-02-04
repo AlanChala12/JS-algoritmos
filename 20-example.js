f97c2ed985938497a653b0ee904925379ebe7a42
Alan Chala <alan.chala12@gmail.com>
2025-02-04 08:54:26 -0500

    Construir una función que reciba como parámetros dos números enteros y retorne el valor del mayor.

---
+++ 20-example.js
@@ -0,0 +1,8 @@
+// Construir una función que reciba como parámetros dos números enteros y retorne el valor del mayor.
+
+const biggest = (int1, int2) => {
+   let desition = int1 > int2 ? int1 : int2
+   return desition
+}
+ 
+biggest( 56, 120 )
