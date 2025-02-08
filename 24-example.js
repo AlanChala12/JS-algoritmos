3340cc5bd7f89666a8e9116240517f308352988d
Alan Chala <alan.chala12@gmail.com>
2025-02-08 12:04:07 -0500

    Construir una función que reciba como parámetro un entero y retorne 1 si dicho valor es múltiplo de 5. Deberá retornar 0 si no es así.

---
+++ 24-example.js
@@ -0,0 +1,8 @@
+// Construir una función que reciba como parámetro un entero y retorne 1 si dicho valor es múltiplo de 5. Deberá retornar 0 si no es así.
+
+function mulOfFive( int ) {
+   return int % 5 == 0 ? 1 : 0;
+}
+ 
+mulOfFive(5);
+ 
