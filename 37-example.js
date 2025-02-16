0cfa35d6d82891244eb67c8abe01df90e98cc689
Alan Chala <alan.chala12@gmail.com>
2025-02-16 12:52:36 -0500

    Mostrar en pantalla todos los números terminados en 6 entre 1 un x número.

---
+++ 37-example.js
@@ -0,0 +1,10 @@
+// Mostrar en pantalla todos los números terminados en 6 entre 1 un x número.
+let int = 100;
+
+for ( let i = 1; i <= int; i++ ) {
+  
+  if ( i % 10 == 6 ) {
+    console.log(i);
+  }
+  
+}
