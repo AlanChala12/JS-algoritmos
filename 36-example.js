5d42a9c2aa188958d5c1a560deb34609f27d6404
Alan Chala <alan.chala12@gmail.com>
2025-02-16 12:48:45 -0500

    Mostrar en pantalla todos los pares comprendidos entre 20 y 200.

--- 36-example.js
+++ 36-example.js
@@ -1,5 +1,10 @@
 // Mostrar en pantalla todos los pares comprendidos entre 20 y 200.
 
+console.log( 'Pares entre 20 y 200' )
 for ( let i = 20; i <= 200; i++ ) {
-  console.log( i );
-}   
+
+  if ( i % 2 == 0 ) {
+    console.log( i )
+  }
+  
+}  
