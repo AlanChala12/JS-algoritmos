585c12dfc5babc0fc45f82b46bb85009cadab6d5
Alan Chala <alan.chala12@gmail.com>
2025-03-03 10:48:53 -0500

    Dados dos números enteros, determinar cuál es el mayor.

---
+++ 52-example.js
@@ -0,0 +1,14 @@
+ // Dados dos números enteros, determinar cuál es el mayor.
+
+ let int1 = 10;
+ let int2 = 10;
+ 
+if ( int1 > int2 ) {
+   console.log(`El número ${ int1 }, ES MAYOR QUE ${ int2 }`);
+} 
+else if ( int2 > int1 ) {
+   console.log(`El número ${ int2 }, ES MAYOR QUE ${ int1 }`);
+} 
+else {
+   console.log(`Ambos números SON IGUALES`);
+}
