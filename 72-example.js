b4ff097e60b647f0e149c34d1ab43ca1d349d876
Alan <alan.chala12@gmail.com>
2025-03-27 10:07:01 -0500

    Determinar cuál es el número menor de un vector

--- 72-example.js
+++ 72-example.js
@@ -1,4 +1,4 @@
-// Determinar cuál es el número menor de un vector.
+// Determinar cuál es el número menor de un vector
 
 let vect = [ 2, 3, 4, 54, 56, 23, 76, 87, 34 ];
 let minNumber = Math.min( ...vect );
