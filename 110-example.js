b643e366ee06c4c8b117845e8f1c636d4da15f62
Alan <alan.chala12@gmail.com>
2025-05-18 14:10:57 -0500

    Determinar si un número es termina en 7

---
+++ 110-example.js
@@ -0,0 +1,9 @@
+// Determinar si un número es termina en 7 
+let number = 270
+
+if ( number % 10 === 7 ) {
+  console.log(`${ number } termina en siete`)
+}
+else {
+  console.log(`${ number } NO termina en siete`)
+}
